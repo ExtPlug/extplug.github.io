6e6ec9aece73c7fd0c3d0250a4877c1af9ef22df
René Kooi <renee@kooi.me>
2017-03-09 13:17:22 +0100

    Add watch mode

--- metalsmith.js
+++ metalsmith.js
@@ -1,16 +1,26 @@
+const opts = require('minimist')(process.argv.slice(2))
+
 const metalsmith = require('metalsmith')
 const layouts = require('metalsmith-layouts')
 const less = require('metalsmith-less')
 const browserify = require('metalsmith-browserify')
+const serve = require('metalsmith-serve')
+const watch = require('metalsmith-watch')
 
-metalsmith(__dirname)
+const compiler = metalsmith(__dirname)
   .use(layouts('handlebars'))
   .use(less())
   .use(browserify({
     dest: 'discover.js',
     entries: ['src/discover/app.js']
   }))
-  .build((err) => {
-    if (err) console.error(err.stack)
-    else console.log('ok')
-  })
+
+if (opts.watch) {
+  compiler.use(serve({ port: opts.port || process.env.PORT || 8080 }))
+  compiler.use(watch())
+}
+
+compiler.build((err) => {
+  if (err) console.error(err.stack)
+  else console.log('ok')
+})
