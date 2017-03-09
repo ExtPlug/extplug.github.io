c88c9f66b574d19bdb141e955abab1784b3b3c3f
René Kooi <renee@kooi.me>
2017-03-09 13:26:10 +0100

    Implement searching for plugins.

--- src/discover/app.js
+++ src/discover/app.js
@@ -1,11 +1,41 @@
 const yo = require('yo-yo')
+const fetch = require('unfetch')
+const debounce = require('debounce')
 
-function Plugin () {
+let searchResults = []
+
+const API_URL = 'https://api.npms.io/v2'
+function search (query) {
+  fetch(`${API_URL}/search?q=keywords:extplug-plugin ${encodeURIComponent(query)}`)
+    .then((response) => response.json())
+    .then((results) => {
+      searchResults = results.results
+      render()
+    })
+}
+
+const searchDebounced = debounce(search, 200)
+
+function onSearchInput (event) {
+  searchDebounced(event.target.value)
+}
+
+function SearchInput () {
+  return yo`
+    <input
+      autofocus
+      class="form-control"
+      type="text"
+      oninput=${onSearchInput} />
+  `
+}
+
+function Plugin ({ package }) {
   return yo`
     <li class="list-group-item">
-      <h4 class="list-group-item-heading">Autowoot</h4>
+      <h4 class="list-group-item-heading">${package.name}</h4>
       <p class="list-group-item-text">
-        ExtPlug plugin that woots every song automatically.
+        ${package.description}
       </p>
       <p class="list-group-item-text">
         <button class="btn btn-primary">Install</button>
@@ -17,8 +47,8 @@ function Plugin () {
 function App () {
   return yo`
     <div class="container">
-      <ul class="list-group">
-        ${Plugin()}
+      <ul class="list-group" style="margin-top: 20px">
+        ${searchResults.map(Plugin)}
       </ul>
     </div>
   `
@@ -31,3 +61,8 @@ function render () {
 const root = document.getElementById('discover')
 const el = App()
 root.appendChild(el)
+
+search('')
+
+const searchRoot = document.getElementById('discover-search')
+searchRoot.appendChild(SearchInput())
