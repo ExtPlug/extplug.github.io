95b66d346c80427f5d97a450d9f41eb38c13feba
René Kooi <renee@kooi.me>
2017-03-09 13:35:30 +0100

    Add babel

--- metalsmith.js
+++ metalsmith.js
@@ -6,13 +6,31 @@ const less = require('metalsmith-less')
 const browserify = require('metalsmith-browserify')
 const serve = require('metalsmith-serve')
 const watch = require('metalsmith-watch')
+const babelify = require('babelify')
+
+const production = process.env.NODE_ENV === 'production'
 
 const compiler = metalsmith(__dirname)
   .use(layouts('handlebars'))
   .use(less())
   .use(browserify({
     dest: 'discover.js',
-    entries: ['src/discover/app.js']
+    entries: ['src/discover/app.js'],
+    transform: [
+      babelify.configure({
+        presets: [
+          ['env', {
+            targets: {
+              uglify: production,
+              browsers: 'last 2 versions'
+            }
+          }]
+        ],
+        plugins: [
+          'yo-yoify'
+        ]
+      })
+    ]
   }))
 
 if (opts.watch) {
